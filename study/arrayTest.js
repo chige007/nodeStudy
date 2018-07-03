718b43b309235899fe280669273d5f9cb3092b5a
System Administrator <root@bookdeMacBook-Air.local>
2018-07-03 13:24:42 +0800

    array

--- study/arrayTest.js
+++ study/arrayTest.js
@@ -43,8 +43,35 @@ console.log(iterator.next());//{ value: [ 0, 'a' ], done: false }
 console.log(iterator.next());//{ value: [ 1, 'b' ], done: false }
 
 console.log('--------------- every (测试数组的所有元素是否都通过了指定函数的测试) ---------------');
-function test(currentValue) {
+function every_test(currentValue) {
     return currentValue < 40;
 }
 var arr_every = [1, 30, 41, 29, 10, 13];
-console.log(arr_every.every(test));
+console.log(arr_every.every(every_test));
+
+console.log('--------------- fill (用一个固定值填充一个数组中从起始索引到终止索引内的全部元素) ---------------');
+// arr.fill(value[, start[, end]])
+// [start, end) 
+var arr_fill = [1,2,3,4,5,6,7,8,9,10].fill(5);
+console.log(arr_fill);//[ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+var arr_fill_2 = [1,2,3,4,5,6,7,8,9,10].fill(5,2,6);
+console.log(arr_fill_2);//[ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+var arr_fill_3 = [1,2,3,4,5,6,7,8,9,10].fill(5,-6,-2);
+console.log(arr_fill_3);//[ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+
+console.log('--------------- filter (创建一个新数组, 其包含通过所提供函数实现的测试的所有元素) ---------------');
+// var new_array = arr.filter(callback(element[, index[, array]])[, thisArg])
+var arr_filter = [1,2,3,4,5,6,7,8,9,10].filter(num => num%2 == 0);
+console.log(arr_filter);
+
+console.log('--------------- find (返回数组中满足提供的测试函数的第一个元素的值。否则返回 undefined) ---------------');
+// arr.find(callback[, thisArg])
+var arr_find = [1,2,3,4,5,6,7,8,9,10].find(num => num%2 == 0);
+console.log(arr_find);
+
+console.log('--------------- findIndex (返回数组中满足提供的测试函数的第一个元素的索引。否则返回-1) ---------------');
+// arr.find(callback[, thisArg])
+var arr_findIndex = ['a','b','c','dd','e','f','g','h','i','j'].findIndex(str => str.length > 1);
+console.log(arr_findIndex);
+
+
